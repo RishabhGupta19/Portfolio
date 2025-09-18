38badb222833ec941fb496326f042138618f450f
RishabhGupta19 <161333480+RishabhGupta19@users.noreply.github.com>
2025-09-19 00:56:43 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -325,13 +325,13 @@ app.get('/api/stats', async (req, res) => {
 });
 
 // Serve static files in production
-if (process.env.NODE_ENV === 'production') {
- // app.use(express.static(path.join(__dirname, 'build')));
+// if (process.env.NODE_ENV === 'production') {
+//  // app.use(express.static(path.join(__dirname, 'build')));
   
-  app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
-}
+//   app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//   });
+// }
 
 // Error handling middleware
 app.use((error, req, res, next) => {
