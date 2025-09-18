6de023342679616e06f392e74bec2f403726f417
RishabhGupta19 <161333480+RishabhGupta19@users.noreply.github.com>
2025-09-19 00:51:48 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -326,7 +326,7 @@ app.get('/api/stats', async (req, res) => {
 
 // Serve static files in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));
+ // app.use(express.static(path.join(__dirname, 'build')));
   
   app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
