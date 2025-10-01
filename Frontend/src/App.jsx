aa5d3607ffacc485556f4cd4d1fa2a7be07bd1b1
RishabhGupta19 <161333480+RishabhGupta19@users.noreply.github.com>
2025-10-01 20:45:59 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 // import React, { useState, useEffect } from 'react';
  import profilePic from '../Profile.jpg';
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 // import { Mail, Phone, MapPin, Github, Linkedin, ExternalLink, Code, Database, Server, Award, GraduationCap, Briefcase, User, Send, Download, Star, Calendar, ChevronRight, Monitor, Globe } from 'lucide-react';
 // <link href="/src/index.css" rel="stylesheet"></link>
 // import { motion } from "framer-motion";
