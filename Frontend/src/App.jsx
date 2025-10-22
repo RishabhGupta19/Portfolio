26601fecfd3b141a143c3728360a716b8f537345
RishabhGupta19 <161333480+RishabhGupta19@users.noreply.github.com>
2025-10-22 14:05:09 +0530

    Update sections array to include 'experience'

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -45,7 +45,7 @@ const Portfolio = () => {
   Docker: <FaDocker className="inline text-blue-400 mr-2" />,
 };
 
-const sections = ['home', 'about', 'skills', 'projects','ai-fitness', 'education', 'certificates', 'contact'];
+const sections = ['home', 'about', 'skills', 'projects','ai-fitness', 'education', 'experience', 'certificates', 'contact'];
 
 useEffect(() => {
   let activeSectionRef = '';
@@ -261,7 +261,7 @@ useEffect(() => {
             </div>
             
             <div className="hidden md:flex space-x-1">
-              {['home', 'about', 'skills', 'projects','ai-fitness' ,'education', 'experience', 'certificates', 'contact'].map((section) => (
+              {['home', 'about', 'skills', 'projects', 'ai-fitness','education', 'experience', 'certificates','contact'].map((section) => (
                 <button
                   key={section}
                   onClick={() => scrollToSection(section)}
