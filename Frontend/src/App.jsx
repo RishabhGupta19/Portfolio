6dae3a97d467dc7225959accae3b42594f2e30fc
RishabhGupta19 <161333480+RishabhGupta19@users.noreply.github.com>
2025-12-20 15:54:52 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -236,7 +236,7 @@ const Portfolio = () => {
             <button onClick={() => scrollToSection('contact')} className=" cursor-pointer w-full sm:w-auto px-8 py-3.5 rounded-full bg-white text-slate-900 font-bold hover:bg-cyan-50 transition-all hover:scale-105 active:scale-95 shadow-[0_0_20px_rgba(255,255,255,0.2)]">
               Let's Talk
             </button>
-            <a href="https://drive.google.com/file/d/1bDD63FSN4eEZqbWLaJrqVWoNw_u7ZOjz/view?usp=sharing" target="_blank" rel="noreferrer" className="w-full sm:w-auto px-8 py-3.5 rounded-full border border-slate-700 hover:border-cyan-500 text-white hover:bg-cyan-500/10 transition-all flex items-center justify-center gap-2">
+            <a href="https://drive.google.com/file/d/15prJAuk0rsp78Oxb6FWQSOyPUffXRdNo/view?usp=drivesdk" target="_blank" rel="noreferrer" className="w-full sm:w-auto px-8 py-3.5 rounded-full border border-slate-700 hover:border-cyan-500 text-white hover:bg-cyan-500/10 transition-all flex items-center justify-center gap-2">
               Download Resume <ExternalLink size={16} className="group-hover:translate-x-1 transition-transform"/>
             </a>
           </div>
